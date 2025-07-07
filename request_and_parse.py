8349f6bea970d67b4f327131087273b67df5075d
Kate Barreiros <kbarreir@redhat.com>
2025-07-07 14:50:32 +0100

    scrapping web data html

---
+++ request_and_parse.py
@@ -0,0 +1,19 @@
+# import relevant libraries
+import requests
+from bs4 import BeautifulSoup
+
+# define the url
+
+url = "https://www.feedbooks.com/search?cat=FBFIC016000&query=feedbooks"
+
+# send a request to get html code from that url
+
+response = requests.get(url, headers={"Accept": "text/html"})
+
+# parse the response
+
+parsed_response = BeautifulSoup(response.text, "html.parser")
+
+# format the parsed HTML response in a way that’s easier to read and print it out
+
+print(parsed_response.prettify())
